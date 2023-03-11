a2cc1f8a07fb0f3660ed0222ed00c548369ca1a2
sequoyah1 <sequoyahschaefer@yahoo.com>
2023-03-11 02:34:01 -0500

    Sequoyah Schaefer Lab7

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,15 +1,44 @@
+/**
+ * Version 1.0
+ * Author Sequoyah Schaefer
+ * Since 3/11/2023
+ */
 import java.util.Scanner;
 
 public class ForLoops {
     private final static Scanner s = new Scanner(System.in);
 
     public static void forSum() {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+            System.out.println("Input Number:");
+            int x = scanner.nextInt();
+            int [] number = {x,36,87,4,6,8};
+            for(int i : number) {
+                sum+= i;
+            }
+            System.out.println(sum);
     }
 
     public static void rangeSum() {
+        int sum = 0;
+        int [] number = {98,36,87,4,6,8};
+        for(int i : number) {
+            sum+= i;
+        }
+        System.out.println(sum);
     }
 
     public static void factorial() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long factorial = 1;
+        for(int i = 1; i <= n; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", n, factorial);
     }
 
     public static void main(String[] args) {
--- src/WhileLoops.java
+++ src/WhileLoops.java
@@ -1,15 +1,80 @@
+/**
+ * Version 1.0
+ * Author Sequoyah Schaefer
+ * Since 3/11/2023
+ */
+
 import java.util.Scanner;
 
 public class WhileLoops {
     private final static Scanner s = new Scanner(System.in);
 
+
+
+
     public static void eoCount() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input:");
+        int n = scanner.nextInt();
+
+        while (n > 1) {
+            if (n % 2 == 0) {
+                System.out.println(n + " " + "=" + "\s" + "Is Even."); }
+            else {
+                System.out.println(n + " " + "=" + "\s" + "Is Odd.");
+
+
+                    }
+            int numbersRead = 0;
+            int sum =0;
+                    while (true) {
+
+                        if (numbersRead == 100) {
+                            break;
+                }
+                        sum = sum + Integer.valueOf(scanner.nextLine());
+                        numbersRead = numbersRead + 1;
+            }
+
+            }
     }
 
     public static void mean() {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        while (true) {
+            if (x < 0) {
+                break;
+            }
+
+        System.out.println("Input 6 total numbers:");
+        float num1 = scanner.nextFloat();
+        float num2 = scanner.nextFloat();
+        float num3 = scanner.nextFloat();
+        float num4 = scanner.nextFloat();
+        float num5 = scanner.nextFloat();
+        float num6 = scanner.nextFloat();
+        float mean;
+        int sum, i;
+        int n = 5;
+        float a[] = {num1,num2,num3,num4,num5,num6};
+        sum = 0;
+
+        for(i = 0; i < n; i++) {
+            sum+=a[i];
+        }
+        System.out.println("Mean ::"+ sum/(float)n);
+
+    }
     }
 
+
+
+
     public static void main(String[] args) {
+
         eoCount();
         mean();
     }
